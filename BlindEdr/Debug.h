3a971602ff5aac477ea37084395b813338a0b35b
Leviathan <github.overplant028@passinbox.com>
2025-01-16 20:44:24 +0800

    Because wsprintfA does not support standard C format descriptors. Let's modify to use sprintf_s

--- BlindEdr/Debug.h
+++ BlindEdr/Debug.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Windows.h>
+#include <stdio.h>
 
 
 // #define DEBUG
@@ -14,15 +15,15 @@ VOID CreateDebugConsole();
 #define GET_FILENAME(path)				(strrchr(path, '\\') ? strrchr(path, '\\') + 1 : path)
 
 
-#define PRINT( STR, ... )                                                                           \
-    if (1) {                                                                                        \
-        LPSTR cBuffer = (LPSTR)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, ERROR_BUF_SIZE);       \
-        if (cBuffer){                                                                               \
-            int iLength = wsprintfA(cBuffer, STR, __VA_ARGS__);                                     \
-            WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), cBuffer, iLength, NULL, NULL);           \
-            HeapFree(GetProcessHeap(), 0x00, cBuffer);                                              \
-        }                                                                                           \
-    }  
+#define PRINT(STR, ...)                                                                          \
+    if (1) {                                                                                     \
+        LPSTR cBuffer = (LPSTR)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, ERROR_BUF_SIZE);   \
+        if (cBuffer) {                                                                           \
+            sprintf_s(cBuffer, ERROR_BUF_SIZE, STR, __VA_ARGS__);                               \
+            WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), cBuffer, (DWORD)strlen(cBuffer), NULL, NULL); \
+            HeapFree(GetProcessHeap(), 0x00, cBuffer);                                          \
+        }                                                                                        \
+    }
 
 #else
     #define PRINT( STR, ... )
